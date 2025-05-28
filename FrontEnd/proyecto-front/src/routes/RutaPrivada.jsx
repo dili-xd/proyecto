7173931cc5345e8a5a1467046633d1147dfe2d55
dili <edajmenesesanarca@gmail.com>
2025-05-28 15:41:45 -0600

    ruta priv

---
+++ FrontEnd/proyecto-front/src/routes/RutaPrivada.jsx
@@ -0,0 +1,14 @@
+
+ function RutaPrivada({children}){
+     function vallidarUsuario() {
+         const Token = localStorage.getItem ('token');
+            if (!Token){
+                return false
+            }
+            return true
+     }
+    return(
+        vallidarUsuario()? children:<h1>NO NO NO  </h1>
+    )
+ }
+ export default RutaPrivada;
